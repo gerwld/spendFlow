b83b7587ab72a94774e529d93c5fa3d6c6ed711c
P Jaworski <pjaworski.dev@gmail.com>
2024-10-06 10:44:03 +0200

    added tabs, fixed layout issues

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
-import React, { useEffect } from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Provider,  useSelector } from 'react-redux';
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Provider, useSelector } from "react-redux";
+import { StatusBar } from "expo-status-bar";
 
-import { Navigation } from 'screens/Navigation';
-import withTranslation from 'hocs/withTranslation';
-import { useInitializeApp, useInitializeAppDemo, useOrientationLock } from 'hooks';
-import { appSelectors, store } from '@redux';
-import i18n from './i18n';
+import { Navigation } from "screens/Navigation";
+import withTranslation from "hocs/withTranslation";
+import { useInitializeAppDemo, useOrientationLock } from "hooks";
+import { appSelectors, store } from "@redux";
+import i18n from "./i18n";
 
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
 
 // keeps the splash screen visible while app fetch resources
 SplashScreen.preventAutoHideAsync();
 
-
 function AppWithProvider({ children }) {
-  useOrientationLock();
-  
-  const lang = useSelector(appSelectors.selectAppLang)
+  // useOrientationLock();
+
+  const lang = useSelector(appSelectors.selectAppLang);
   // useInitializeApp(lang);
   useInitializeAppDemo(lang);
 
   useEffect(() => {
     i18n.changeLanguage(lang);
-  }, [lang])
+  }, [lang]);
 
   return children;
 }
@@ -33,11 +32,11 @@ export default withTranslation(function RootComponent() {
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-      <AppWithProvider>
-        <Navigation />
-        <StatusBar translucent style="light" />
-      </AppWithProvider>
+        <AppWithProvider>
+          <Navigation />
+          <StatusBar translucent style="light" />
+        </AppWithProvider>
       </GestureHandlerRootView>
     </Provider>
-  )
-})
+  );
+});
---
+++ src/screens/home/Week.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Week = () => {
+  return (
+    <View>
+      <Text>Week</Text>
+    </View>
+  );
+};
+
+export default Week;
