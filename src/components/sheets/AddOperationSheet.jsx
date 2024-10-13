c374c8c1c622f2550dd500228d9c51dc2994646f
P Jaworski <pjaworski.dev@gmail.com>
2024-10-13 15:33:05 +0200

    Added select: Category, Account, AddTitle, Calendar

--- src/components/sheets/AddOperationSheet.jsx
+++ src/components/sheets/AddOperationSheet.jsx
@@ -7,16 +7,44 @@ import { useCurrentTheme } from 'hooks';
 import SegmentedControl from "react-native-segmented-control-2";
 import ActionSheetExperimental from './ActionSheetExperimental';
 import { LineItemView, Calendar } from '@components';
-import { LucideApple, LucideBookHeart, LucideCalendar,  LucidePopcorn,  PenBoxIcon } from 'lucide-react-native';
+import { LucideCalendar, PenBoxIcon } from 'lucide-react-native';
 import { produce } from 'immer';
+import CategoryItem from '../items/CategoryItem';
+import { LucideApple, LucidePopcorn, LucidePlus, LucideBookHeart, LucideTrain, LucideCat, Landmark, ShieldCheck, ArrowBigDownDash, EthernetPort } from 'lucide-react-native';
+
 
 const isFirstPlusOrMinus = (value) => value[0] === "-" || value[0] === "+"
+
+const accountsArray = [
+  { id: 1, title: 'Entertainment', icon: <LucidePopcorn stroke="#ff3939" />, iconColor: '#ff3939' },
+  { id: 2, title: 'Groceries', icon: <LucideApple stroke="#3988ff" />, iconColor: '#3988ff' },
+  { id: 3, title: 'Education', icon: <LucideBookHeart stroke="#ff8c39" />, iconColor: '#ff8c39' },
+  { id: 10, title: 'Add new', icon: <LucidePlus stroke="#b3b9bf" />, iconColor: '#b3b9bf', isAddNew: true },
+];
 
 const categoriesArray = [
   { id: 1, title: 'Entertainment', icon: <LucidePopcorn stroke="#ff3939" />, iconColor: '#ff3939' },
   { id: 2, title: 'Groceries', icon: <LucideApple stroke="#3988ff" />, iconColor: '#3988ff' },
   { id: 3, title: 'Education', icon: <LucideBookHeart stroke="#ff8c39" />, iconColor: '#ff8c39' },
+  { id: 4, title: 'Transport', icon: <LucideTrain stroke="#39ff6e" />, iconColor: '#39ff6e' },
+  { id: 5, title: 'Savings', icon: <Landmark stroke="#3999ff" />, iconColor: '#3999ff' },
+  { id: 6, title: 'Pets', icon: <LucideCat stroke="#decd36" />, iconColor: '#decd36' },
+  { id: 7, title: 'Debt Payments', icon: <ArrowBigDownDash stroke="#ff39ff" />, iconColor: '#ff39ff' },
+  { id: 8, title: 'Insurance', icon: <ShieldCheck stroke="#31e2cd" />, iconColor: '#31e2cd' },
+  { id: 9, title: 'Subscriptions', icon: <EthernetPort stroke="#5236f4" />, iconColor: '#5236f4' },
+  { id: 10, title: 'Add new', icon: <LucidePlus stroke="#b3b9bf" />, iconColor: '#b3b9bf', isAddNew: true },
 ];
+
+const accStyles = StyleSheet.create({
+  pageExpensesContent: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    gap: 10,
+    rowGap: 12,
+    marginHorizontal: 15,
+  },
+});
 
 
 const AddOperationSheet = ({ isOpen, toggleSheet }) => {
@@ -38,6 +66,15 @@ const AddOperationSheet = ({ isOpen, toggleSheet }) => {
 
   const toggleCalendar = () => {
     dispatchAction("isCalendarSheet", !state.isCalendarSheet)
+  }
+  const toggleAccount = () => {
+    dispatchAction("isAccountSheet", !state.isAccountSheet)
+  }
+  const toggleCategory = () => {
+    dispatchAction("isCategorySheet", !state.isCategorySheet)
+  }
+  const toggleTitle = () => {
+    dispatchAction("isTitleSheet", !state.isTitleSheet)
   }
 
 
@@ -199,25 +236,27 @@ const AddOperationSheet = ({ isOpen, toggleSheet }) => {
           }} />
       </View>
       <View style={[styles.valueBlock, styles.extraGap]}>
-        <Pressable style={{ width: "100%" }} onPress={() => openURL('https://docs.google.com/forms/d/e/1FAIpQLSd2chzzyUdExwXJXpBCHVP3JnU8nUETBQbe54DWoMWbHBHT_g/viewform?usp=sf_link')}>
+        <Pressable style={{ width: "100%" }} onPress={toggleAccount}>
           <LineItemView isOperation pl1 isLastItem rightArrow>
-            <ItemViewIcon 
-              {...{...categoriesArray[2],
+            <ItemViewIcon
+              {...{
+                ...categoriesArray[2],
                 defBackground: themeColors.bgHighlightSec,
                 theme: themeColors.label
-              }}/>
+              }} />
             <Text style={styles.selectItemText}>Select Account</Text>
           </LineItemView>
         </Pressable>
       </View>
       <View style={styles.valueBlock}>
-        <Pressable style={{ width: "100%" }} onPress={() => openURL('https://docs.google.com/forms/d/e/1FAIpQLSd2chzzyUdExwXJXpBCHVP3JnU8nUETBQbe54DWoMWbHBHT_g/viewform?usp=sf_link')}>
+        <Pressable style={{ width: "100%" }} onPress={toggleCategory}>
           <LineItemView isOperation pl1 isLastItem rightArrow>
-            <ItemViewIcon 
-              {...{...categoriesArray[0],
+            <ItemViewIcon
+              {...{
+                ...categoriesArray[0],
                 defBackground: themeColors.bgHighlightSec,
                 theme: themeColors.label
-              }}/>
+              }} />
             <Text style={styles.selectItemText}>Select Category</Text>
           </LineItemView>
         </Pressable>
@@ -225,32 +264,34 @@ const AddOperationSheet = ({ isOpen, toggleSheet }) => {
       <View style={styles.valueBlock}>
         <Pressable style={{ width: "100%" }} onPress={toggleCalendar}>
           <LineItemView isOperation pl1 isLastItem rightArrow>
-          <ItemViewIcon 
+            <ItemViewIcon
               {...{
-                icon: <LucideCalendar stroke={themeColors.textColor} width={25} height={25}/>,
+                icon: <LucideCalendar stroke={themeColors.textColor} width={25} height={25} />,
                 defBackground: themeColors.bgHighlightSec,
                 theme: themeColors.label
-              }}/>
-            
+              }} />
+
             <Text style={styles.selectItemText}>Friday, 16 February</Text>
           </LineItemView>
-          </Pressable>
+        </Pressable>
       </View>
 
       <View style={styles.valueBlock}>
+        <Pressable style={{ width: "100%" }} onPress={toggleTitle}>
           <LineItemView isOperation pl1 isLastItem rightArrow>
-          <ItemViewIcon 
+            <ItemViewIcon
               {...{
-                icon: <PenBoxIcon stroke={themeColors.textColor} width={24} height={24}/>,
+                icon: <PenBoxIcon stroke={themeColors.textColor} width={24} height={24} />,
                 defBackground: themeColors.bgHighlightSec,
                 theme: themeColors.label
-              }}/>
-            
+              }} />
+
             <Text style={styles.selectItemText}>Add Title</Text>
-            <Text style={[styles.selectItemText, {opacity: 0.5, marginLeft: 2}]}> (not required)</Text>
+            <Text style={[styles.selectItemText, { opacity: 0.5, marginLeft: 2 }]}> (not required)</Text>
           </LineItemView>
+        </Pressable>
       </View>
-    
+
 
       {/* ----------------------------- SHEETS PART ---------------------------- */}
       <ActionSheetExperimental {...{
@@ -270,7 +311,24 @@ const AddOperationSheet = ({ isOpen, toggleSheet }) => {
         {...{
           isOpen: state.isCalendarSheet,
           toggleSheet: toggleCalendar
-        }}/>
+        }} />
+
+      <AccountSheet
+        {...{
+          isOpen: state.isAccountSheet,
+          toggleSheet: toggleAccount
+        }} />
+
+      <CategorySheet
+        {...{
+          isOpen: state.isCategorySheet,
+          toggleSheet: toggleCategory
+        }} />
+      <TitleSheet
+        {...{
+          isOpen: state.isTitleSheet,
+          toggleSheet: toggleTitle
+        }} />
     </ScrollView>
   )
 
@@ -282,7 +340,7 @@ const AddOperationSheet = ({ isOpen, toggleSheet }) => {
       {...{
         title: "Add operation",
         isOpen, toggleSheet,
-        rightButton: {title: "Save", onPress: () => alert("save pressed.")},
+        rightButton: { title: "Save", onPress: () => alert("save pressed.") },
         backgroundColor: themeColors.bgHighlight
       }}>
       {renderContent}
@@ -296,33 +354,158 @@ const AddOperationSheet = ({ isOpen, toggleSheet }) => {
 
 }
 
+const TitleSheet = ({ isOpen, toggleSheet }) => {
+  const [themeColors] = useCurrentTheme();
+  const [value, setValue] = React.useState("");
+  const inputRef = React.useRef(null);
 
-const CalendarSheet = ({isOpen, toggleSheet}) => {
+  const styles = StyleSheet.create({
+    titleInput: {
+      width: "100%",
+      height: 45,
+      maxHeight: 45,
+      marginBottom: "auto",
+      fontSize: 20,
+      paddingHorizontal: 15,
+      color: themeColors.textColor,
+      fontWeight: "500",
+      textAlign: "left",
+    },
+    valueBlock: {
+      alignItems: 'center',
+      flexDirection: "row",
+      justifyContent: 'center',
+      padding: 4,
+      marginTop: 12,
+      marginHorizontal: 20,
+      borderRadius: 10,
+      backgroundColor: themeColors.activeArea,
+    },
+  });
+
+  
+  React.useEffect(() => {
+    if (isOpen) {
+      const timer = setTimeout(() => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+        }
+      }, 300);
+
+      return () => clearTimeout(timer); // Cleanup timer if the component is unmounted or isOpen changes
+    }
+  }, [isOpen]);
+
+  return (
+    <BottomSheetExperimental
+      {...{
+        leftButton: { title: "Back", onPress: toggleSheet },
+        rightButton: { title: "Save", onPress: () => alert("value: " + value) },
+        setFullWidth: true,
+        maxHeightMultiplier: 0.60,
+        setHeight: 500,
+        scrollable: true,
+        title: "Add Title",
+        isOpen,
+        toggleSheet,
+        backgroundColor: themeColors.bgHighlight
+      }}>
+
+      <View style={styles.valueBlock}>
+        <TextInput
+          ref={inputRef}
+          placeholder="New sneakers..."
+          maxLength={10}
+          style={styles.titleInput}
+          value={value}
+          onChangeText={setValue}
+        />
+      </View>
+
+    </BottomSheetExperimental>
+  );
+};
+
+
+
+const AccountSheet = ({ isOpen, toggleSheet }) => {
   const [themeColors] = useCurrentTheme();
 
-  const onDayPress =() => {
+  return (
+    <BottomSheetExperimental
+      {...{
+        leftButton: { title: "Back", onPress: toggleSheet },
+        setFullWidth: true,
+        maxHeightMultiplier: 0.50,
+        setHeight: 410,
+        scrollable: true,
+        title: "Select Account",
+        isOpen, toggleSheet,
+        backgroundColor: themeColors.bgHighlight
+      }}>
 
-  }
-  
+      <View style={accStyles.pageExpensesContent}>
+        {accountsArray.map(item => (
+          <CategoryItem key={item.id} iconColor={item.iconColor} icon={item.icon} title={item.title} isRow />
+        ))}
+      </View>
+
+    </BottomSheetExperimental>
+  )
+}
+
+const CategorySheet = ({ isOpen, toggleSheet }) => {
+  const [themeColors] = useCurrentTheme();
+
+  return (
+    <BottomSheetExperimental
+      {...{
+        leftButton: { title: "Back", onPress: toggleSheet },
+        setFullWidth: true,
+        maxHeightMultiplier: 0.5,
+        setHeight: 900,
+        scrollable: true,
+        title: "Select Category",
+        isOpen, toggleSheet,
+        backgroundColor: themeColors.bgHighlight
+      }}>
+
+      <View style={accStyles.pageExpensesContent}>
+        {categoriesArray.map(item => (
+          <CategoryItem key={item.id} iconColor={item.iconColor} icon={item.icon} title={item.title} isRow />
+        ))}
+      </View>
+
+    </BottomSheetExperimental>
+  )
+}
+
+const CalendarSheet = ({ isOpen, toggleSheet }) => {
+  const [themeColors] = useCurrentTheme();
+  const [value, setValue] = React.useState(null);
+
+  const onDayPress = () => { }
+
   return (
     <BottomSheet
-    {...{
-      leftButton: {title: "Back", onPress: toggleSheet},
-      setFullWidth: true,
-      maxHeightMultiplier: 0.50,
-      setHeight: 410,
-      scrollable: true,
-      title: "Select Date",
-      isOpen, toggleSheet,
-      backgroundColor: themeColors.bgHighlight
-    }}>
-    <Calendar
-      borderColor={themeColors.calendarBorderColor}
-      color={themeColors.textColor}
-      colorContrast={themeColors.textColorHighlight}
-      itemID={null}
-      activeColor={themeColors.tabsActiveColor}
-      onChange={onDayPress} />
+      {...{
+        leftButton: { title: "Back", onPress: toggleSheet },
+        rightButton: { title: "Save", onPress: () => alert("value: " + value) },
+        setFullWidth: true,
+        maxHeightMultiplier: 0.50,
+        setHeight: 410,
+        scrollable: true,
+        title: "Select Date",
+        isOpen, toggleSheet,
+        backgroundColor: themeColors.bgHighlight
+      }}>
+      <Calendar
+        borderColor={themeColors.calendarBorderColor}
+        color={themeColors.textColor}
+        colorContrast={themeColors.textColorHighlight}
+        itemID={null}
+        activeColor={themeColors.tabsActiveColor}
+        onChange={onDayPress} />
     </BottomSheet>
   )
 }
@@ -331,8 +514,8 @@ const CalendarSheet = ({isOpen, toggleSheet}) => {
 
 
 
-const ItemViewIcon = ({icon, iconColor, defBackground, theme}) => {
-  
+const ItemViewIcon = ({ icon, iconColor, defBackground, theme }) => {
+
   const styles = StyleSheet.create({
     icon: {
       width: 45,
@@ -355,10 +538,10 @@ const ItemViewIcon = ({icon, iconColor, defBackground, theme}) => {
   return (
     <View style={styles.icon}>
       {icon}
-      <View style={styles.icon_bg}/>
+      <View style={styles.icon_bg} />
     </View>
   )
-  
+
 }
 
 
